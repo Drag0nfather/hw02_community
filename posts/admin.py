d0d78faffbb59064ddcc2ed0fe73c1ea29ce0015
Andrew <Andrey18651@gmail.com>
2020-10-16 19:14:34 +0300

    new commit

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("pk", "text", "pub_date", "author") 
@@ -9,3 +9,4 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 admin.site.register(Post, PostAdmin) 
+admin.site.register(Group)
--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("group/<slug>/", views.group_posts),
+    path("", views.index, name="index"),
+    path("test/", views.group_posts, name="group-posts")
 ] 
