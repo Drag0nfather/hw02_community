56f3a03f26da740f3818b64d6cf8ca0711c68dfb
Andrew <Andrey18651@gmail.com>
2020-11-15 21:57:12 +0300

    last commit in 3 sprint

--- posts/models.py
+++ posts/models.py
@@ -17,6 +17,7 @@ class Group(models.Model):
 class Post(models.Model):
     class Meta:
         ordering = ["-pub_date"]
+
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
